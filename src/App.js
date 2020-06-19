06aaea873b15d2abe9b9f2a5cbf2901e12a73a1a
pranay <pranaythotaece@gmail.com>
2020-06-19 12:15:25 -0400

    added  card details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from "./Card/card-list-component.jsx";
+import "./App.css";
+import Searchbox from "./search-box/search-box.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      restaurants: [],
+      searchField:``,
+    };
+
+    this.baseUrl = `http:\/\/opentable.herokuapp.com\/api\/restaurants?`;
+  this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  this.fetchResults = this.fetchResults.bind(this);
+   this.onSearchSubmit('toronto');
+  };
+
+
+  
+
+
+  onSearchSubmit(value){
+    // using the city param for more interesting results to play with
+    this.url = this.baseUrl + `city=${value}`;
+
+    this.fetchResults(this.url);
+}
+
+
+fetchResults(url){
+  fetch(url)
+  .then((response) => response.json())
+      .then((restaurant) =>
+        this.setState({ restaurants: restaurant.restaurants })
+      );
+}
+
+  // componentDidMount() {
+  //   fetch(
+  //     `http:\/\/opentable.herokuapp.com\/api\/restaurants?` +
+  //       `city=${`toronto`}`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((restaurant) =>
+  //       this.setState({ restaurants: restaurant.restaurants })
+  //     );
+  // }
+
+
+
+
+
+  render() {
+
+    const{restaurants,searchField}=this.state;
+    const filterRestaurants = restaurants.filter(restaurant=>restaurant.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+      <Searchbox onChange={e=>this.setState({searchField:e.target.value})} placeholder={`Enter your city`} onClick={this.state.searchField}/>
+        <CardList restaurants={filterRestaurants} />
+      </div>
+    );
+  }
 }
 
 export default App;
