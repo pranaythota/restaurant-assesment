e61ac2fc7fa47aff8213956363bce598859c237d
pranay <pranaythotaece@gmail.com>
2020-06-21 14:53:01 -0400

    added all files

--- src/App.js
+++ src/App.js
@@ -1,68 +1,57 @@
 import React, { Component } from "react";
-import CardList from "./Card/card-list-component.jsx";
 import "./App.css";
-import Searchbox from "./search-box/search-box.jsx";
+import Searchbox from "./components/search-box/search-box.jsx";
+import Restuarents from "./views/Restuarents.js";
+import { searchRestaurents ,filterRestuarents} from "./actions/RestuarentActions";
+import { connect } from "react-redux";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      restaurants: [],
-      searchField:``,
-    };
-
-    this.baseUrl = `http:\/\/opentable.herokuapp.com\/api\/restaurants?`;
-  this.onSearchSubmit = this.onSearchSubmit.bind(this);
-  this.fetchResults = this.fetchResults.bind(this);
-   this.onSearchSubmit('toronto');
+  state = { city: "" ,searchField:""};
+  searchResturant = () => {
+    this.props.searchRestaurents(this.state.city);
   };
 
+  handleKeyDown = (event) => {
+    if ((event.which === 13 || event.keyCode === 13) && this.state.city !== "") {
+      this.props.searchRestaurents(this.state.city);
+    }
+  };
+  handCityInputChange = (event) => {
+    
+    this.setState({ city: event.target.value });
+  };
 
-  
+  handleFilterSearch = (event) => {
+    
+    this.setState({ searchField: event.target.value });
+  } 
 
-
-  onSearchSubmit(value){
-    // using the city param for more interesting results to play with
-    this.url = this.baseUrl + `city=${value}`;
-
-    this.fetchResults(this.url);
-}
-
-
-fetchResults(url){
-  fetch(url)
-  .then((response) => response.json())
-      .then((restaurant) =>
-        this.setState({ restaurants: restaurant.restaurants })
-      );
-}
-
-  // componentDidMount() {
-  //   fetch(
-  //     `http:\/\/opentable.herokuapp.com\/api\/restaurants?` +
-  //       `city=${`toronto`}`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((restaurant) =>
-  //       this.setState({ restaurants: restaurant.restaurants })
-  //     );
-  // }
-
-
-
-
+  filterRestuarents = () => {
+    if(this.state.searchField ==='' && this.state.city !== "" )
+        this.props.searchRestaurents(this.state.city)
+    this.props.filterRestuarents(this.state.searchField)
+  }
 
   render() {
-
-    const{restaurants,searchField}=this.state;
-    const filterRestaurants = restaurants.filter(restaurant=>restaurant.name.toLowerCase().includes(searchField.toLowerCase()))
+    console.log(this.state);
     return (
       <div className="App">
-      <Searchbox onChange={e=>this.setState({searchField:e.target.value})} placeholder={`Enter your city`} onClick={this.state.searchField}/>
-        <CardList restaurants={filterRestaurants} />
+        <Searchbox
+          name="city"
+          handleChange={this.handCityInputChange}
+          placeholder="Enter your city"
+          handleKeyDown={this.handleKeyDown}
+          onSubmit={this.searchResturant}
+        />
+        <Searchbox
+          handleChange={this.handleFilterSearch}
+          placeholder={`Search by Name address or area`}
+          onSubmit={this.filterRestuarents}
+        />
+        <Restuarents />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, { searchRestaurents,filterRestuarents })(App);
